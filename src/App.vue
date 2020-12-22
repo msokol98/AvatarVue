a2b27a46b584facb4fec8f45e204b0c62fca1e1a
Mitchell Sokol <msokol98@gmail.com>
2020-12-22 11:33:51 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Custom />
+  </div>
+</template>
+
+<script>
+import Custom from './components/Custom.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Custom
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
